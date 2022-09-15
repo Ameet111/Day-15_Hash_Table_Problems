bee2e931c652d25c0e9228e677d08575a83b17ec
Ameet Jadhav <ammugs11@gmail.com>
2022-09-15 15:43:01 +0530

    added find index of the words in the para

---
+++ src/com/bridgelabhashtableuc2/MyLinkedList.java
@@ -0,0 +1,40 @@
+package com.bridgelabhashtableuc2;
+
+public class MyLinkedList<K> {
+	public INode<K> head;
+	public INode tail;
+
+	public MyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+
+	public INode<K> search(K key) {
+		INode<K> currentNode = head;
+		while (currentNode != null &&currentNode.getNext() != null) {
+			if (currentNode.getKey().equals(key)) {
+				return currentNode;
+			}
+			currentNode = currentNode.getNext();
+		}
+		return null;
+	}
+	
+	public void append(INode<K> currentNode) {
+		if (this.tail == null)
+			this.tail = currentNode;
+		if (this.head == null) {
+			this.head = currentNode;
+		} else {
+			this.tail.setNext(currentNode);
+			this.tail =currentNode;
+		}
+	}
+
+	@Override
+	public String toString() {
+			return "MyLinkedList [head=" + head + ", tail=" + tail + "]";
+		}
+ 
+
+}
